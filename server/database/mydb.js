f0addc0841c7f1c20be0b14147cb7df379986ecd
adesh <officialadesh0001@gmail.com>
2025-10-30 04:58:44 +0530

    first website

--- server/database/mydb.js
+++ server/database/mydb.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-mongoose.connect(process.env.MYDATAPATH, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   connectTimeoutMS: 10000,
